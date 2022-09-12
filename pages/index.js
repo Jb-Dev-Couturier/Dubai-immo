aae89becc9477fa23eb3ba6c9b918c314502ddfc
JB-Dev <jibaoke@gmail.com>
2022-09-12 09:39:08 +0200

    init project

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Flex,Box,Button,Text } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>ImmoRicain Agence Mondial</title>
+        <meta name="description" content="Agence mondial spécialisé dans la location ou l'achat de propriété au UAE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        
+      </main>
+    </div>
+  )
+}
